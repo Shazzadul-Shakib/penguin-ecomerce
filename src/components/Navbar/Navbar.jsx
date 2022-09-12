8b23a61a5bd3d4904a1166e56a842afcb1b5b05a
Shazzadul-Shakib <shakib1186@gmail.com>
2022-09-12 23:39:56 +0600

    padding correction in navbar top

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../assets/landing_page/Group 33072.png';
 const Navbar = () => {
   return (
-    <div className=' flex justify-between items-center mx-4 sm:mx-[135px] py-10 text-[#3C3C3C] font-medium'>
+    <div className=' flex justify-between items-center mx-4 py-10 text-[#3C3C3C] font-medium md:mx-6 lg:mx-[135px]'>
       <img className='w-50 h-8 sm:w-60 sm:h-10' src={logo} alt="Penguin Com" />
       <ul className=' hidden sm:flex '>
         <li className=' ml-14'><a href="home">Home</a></li>
